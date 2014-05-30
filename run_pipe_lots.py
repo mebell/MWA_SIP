724185e4d4894ce7f87e7925b767d2a16b78484e
Martin Bell <martinbell81@googlemail.com>
2014-05-30 10:48:57 +1000

    tidying ip paths in run_pipe_lots.py

--- run_pipe_lots.py
+++ run_pipe_lots.py
@@ -3,8 +3,36 @@ import re
 
 ################################################################################
 # File to create and submit multiple job files for downloading and processing. #
+# This script stages the data reduction so the storage area does not fill up.  # 
 ################################################################################
 
+####################################################
+#Parset files path(s)
+loc_parset_file    = '/Users/bel27a/Work/MWA_SIP/loc_parset.txt'
+#####################################################
+
+def read_parset(parset_file):
+    """
+     Read the parameter file defined in parset file path and return args
+    """
+    print 'Reading parameter set file'
+    f = open(parset_file,'r')
+    config_dict = {}
+    for line in f:
+        if line[0] == '#':
+            pass;
+        else:
+            items = line.split('=', 1)
+            config_dict[items[0]] = eval(items[1])
+    return config_dict
+
+####################################################
+# Read the location parset to find the paths
+locs = read_parset(loc_parset_file)
+SIP_home= = locs['SIP_home']
+error_logs = locs['error_logs']
+
+####################################################
 
 def run_job(id, deps):
     print '#####################################################'
@@ -13,13 +41,13 @@ def run_job(id, deps):
     job_file = open('download.go', 'a')
     print 'Writing appropriate download download file for observation ' 
     # Add the appropriate command. 
-    job_file.write('python /home/562/meb562/SIP/get_data.py '+id)
+    job_file.write('python '+SIP_home'+/get_data.py '+id)
     job_file.close()
     ####### Error Logs ########
     name = id.split('/')[-1]        # Define the output and error log names
     name = re.split("[\r\n]+",name)[0] # Get rid of the return carriage
-    error_log = '/home/562/meb562/SIP/error_logs/'+name+'.download.error'
-    output_log = '/home/562/meb562/SIP/error_logs/'+name+'.download.output'
+    error_log =  error_logs+'/'+name+'.download.error'
+    output_log = error_logs+'/'+name+'.download.output'
     ######## Normal Que ########
     if len(deps)==0:       
        print 'Submitting download to qsub NORMAL'
@@ -45,23 +73,23 @@ def run_job(id, deps):
     job_file = open('job_file.go', 'a')
     print 'Will only exceute qsub job when download is complete' 
     # Add the appropriate command.  
-    job_file.write('python /home/562/meb562/SIP/pre_proc_pipe.py '+id)
+    job_file.write('python '+SIP_home+'/pre_proc_pipe.py '+id)
     job_file.close()
     # Submit the file to qsub.
     name = id.split('/')[-1]        # Define the output and error log names
     name = re.split("[\r\n]+",name)[0] # Get rid of the return carriage
-    error_log = '/home/562/meb562/SIP/error_logs/'+name+'.proc.error'
-    output_log = '/home/562/meb562/SIP/error_logs/'+name+'.proc.output'
+    error_log =  error_logs+'/'+name+'.proc.error'
+    output_log = error_logs+'/'+name+'.proc.output'
     print 'Data reduction command is = qsub -W depend=afterok:'+term_out+' -e '+error_log+' -o '+output_log+' job_file.go'
     print 'Submitting jobfile'
     os.system('qsub -W depend=afterok:'+term_out+' -e '+error_log+' -o '+output_log+' job_file.go')
     print '#####################################################'
     return sub_jobs
 
+############## Main loop ###############
 # Work out which GPS ID / datafile we are going to work on 
-id_file = open('obs_id_list.txt', 'r')
 
-############################
+id_file = open(SIP_home+'/obs_id_list.txt', 'r')
 
 sub_jobs = []
 count = 0
@@ -75,17 +103,5 @@ for line in id_file:
        run_job(line,deps=sub_jobs[28:55])
     count = count + 1
 
-
-
-
-
-
-
-
-
-
-
-   
-     
 
 
